971a24e0f1ec6a97ecff2518d2a381ec215ee3b6
Enrico Bianchi <enrico.bianchi@gmail.com>
2023-05-11 00:39:18 +0200

    Added missing module-info and package-info informations (#37)

--- src/main/java/io/github/jopenlibs/vault/api/sys/package-info.java
+++ src/main/java/io/github/jopenlibs/vault/api/sys/package-info.java
@@ -1,8 +1,9 @@
 /**
- * <p>Classes implementing the various endpoints of the Vault HTTP API.</p>
+ * <p>Intended for internal use by other classes within the Vault driver library.</p>
  *
- * <p>The classes in this package are not meant to be instantiated directly.  Rather, they should
- * be used by way of the <code>io.github.jopenlibs.vault.Vault</code> driver class, in a DSL-style
- * builder pattern approach.</p>
+ * <p>Classes in this package implements the /sys/ API endpoints. There are not meant to be
+ * instantiated directly. Rather, they should be used by way of the
+ * <code>io.github.jopenlibs.vault.Vault</code> driver class, in a DSL-style builder pattern
+ * approach.</p>
  */
 package io.github.jopenlibs.vault.api.sys;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module vault.java.driver {
     requires java.logging;
     exports io.github.jopenlibs.vault;
     exports io.github.jopenlibs.vault.api;
+    exports io.github.jopenlibs.vault.api.database;
+    exports io.github.jopenlibs.vault.api.pki;
     exports io.github.jopenlibs.vault.api.sys;
+    exports io.github.jopenlibs.vault.api.sys.mounts;
     exports io.github.jopenlibs.vault.json;
     exports io.github.jopenlibs.vault.response;
     exports io.github.jopenlibs.vault.rest;
